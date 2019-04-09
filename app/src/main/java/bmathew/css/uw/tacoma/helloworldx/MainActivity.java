9722288d372331271e35e984a8b527be49b50823
Spiritoak <44207781+Spiritoak@users.noreply.github.com>
2019-04-08 23:47:43 -0700

    Initial Version

---
+++ app/src/main/java/bmathew/css/uw/tacoma/helloworldx/MainActivity.java
@@ -0,0 +1,13 @@
+package bmathew.css.uw.tacoma.helloworldx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
